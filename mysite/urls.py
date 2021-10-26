69fe570abd27cb49a80a344f036bf48f524a5dcf
masuod <masuod.abbasian@gmail.com>
2021-10-26 15:08:26 -0700

    create blog app
    create blog app.
    set template with static file.
    set address in index.html , contact.html , about.html.
    create base.html.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('website.urls'))
+    path('', include('website.urls')),
+    path('blog/',include('blog.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
