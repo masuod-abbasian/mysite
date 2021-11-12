3bfc20cb38158f4090dcfef21719e03dd0e5f3da
masuod <masuod.abbasian@gmail.com>
2021-11-12 02:55:54 -0800

    admin, Meta

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class POST(models.Model):
+    # image
+    # author
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    # category
+    # tag
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_date']
+        
+    def __str__(self):
+        return "{} - {}".format(self.title,self.id)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=250)
+    email = models.EmailField()
+    massage = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
