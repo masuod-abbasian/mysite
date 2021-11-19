16bf9932bfd57e50b98cc457c80d875513102fe7
masuod <masuod.abbasian@gmail.com>
2021-11-19 04:44:24 -0800

    finall tamrin 6_2

--- blog/views.py
+++ blog/views.py
@@ -11,29 +11,40 @@ def blog_single(request,pid):
     post = POST.objects.filter(status=1)
     posts = get_object_or_404(post, pk=pid)
     post_index = list(post.values_list('pk',flat=True))
+    
     # post_index = list(POST.objects.filter(status=1))
     # pid = int(pid)
     post_i = post_index.index(pid)
+    try:
+        id_prev = post[post_i+1]
+    except:
+        id_prev = post[post_i]
+    
+    try:
+        id_next = post[post_i-1]
+    except:
+        id_next = post[post_i]
     # if post_i <= len(post_index):
     # try:
-    if post_i >= len(post_index) :
-        id_next = post_index[post_i+1]
+    # if post_i >= len(post_index) :
+    #     id_next = post_index[post_i+1]
     # except IndexError:
         # posts = get_object_or_404(post, pk=pid)    
     
-    else:
+    # else:
         
-        id_next = pid
+    #     id_next = pid
     # if post_i >= len(post_index) :
     # try:
-    if post_i <= len(post_index):
-        id_prev = post_index[post_i-1]
+    # if post_i <= len(post_index):
+    #     id_prev = post_index[post_i-1]
+        
         
     # except IndexError:
         # posts = get_object_or_404(post, pk=pid)     
-    else:
+    # else:
         
-        id_prev = pid
+    #     id_prev = pid
     
     # else:
         # id_prev = pid
